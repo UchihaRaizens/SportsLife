2664dfc422aa6621103de92e7cc528233852ade0
lresutik <lresutik@gmail.com>
2019-09-10 13:47:10 +0200

    Add post method for save user account

--- sportslife-server/src/main/java/com/example/sportslife/userServices/UserAccountController.java
+++ sportslife-server/src/main/java/com/example/sportslife/userServices/UserAccountController.java
@@ -1,9 +1,13 @@
 package com.example.sportslife.userServices;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +24,10 @@ public class UserAccountController {
 		UserAccount userAccount = userAccountRepository.findById(userAccountId)
 				.orElseThrow(() -> new ResourceNotFoundException ("User account not found :: " + userAccountId));
 		return ResponseEntity.ok().body(userAccount);
+	}
+	
+	@PostMapping("/user")
+	public UserAccount createUserAccount(@Valid @RequestBody UserAccount userAccount) {
+		return userAccountRepository.save(userAccount);
 	}
 }
